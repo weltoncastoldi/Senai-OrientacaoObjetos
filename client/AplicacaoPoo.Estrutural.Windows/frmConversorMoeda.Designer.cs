7e6f358f7eb864127c4a9794177b0fa37d641f2e
Welton Castoldi <weltoncastoldi@hotmail.com>
2022-09-21 21:50:43 -0300

    refactor: Criada Classe de conversão de dados, e refatorei frmConversor de moedas. Fix: Corrigido erro do txtValorEmDolar quando ficava em branco;

--- client/AplicacaoPoo.Estrutural.Windows/frmConversorMoeda.Designer.cs
+++ client/AplicacaoPoo.Estrutural.Windows/frmConversorMoeda.Designer.cs
@@ -28,19 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtCotacaoDolar = new System.Windows.Forms.TextBox();
             this.txtValorEmDolar = new System.Windows.Forms.TextBox();
             this.btnConverterEmReal = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // txtCotacaoDolar
-            // 
-            this.txtCotacaoDolar.Location = new System.Drawing.Point(33, 29);
-            this.txtCotacaoDolar.Name = "txtCotacaoDolar";
-            this.txtCotacaoDolar.PlaceholderText = "cotação do dolar";
-            this.txtCotacaoDolar.Size = new System.Drawing.Size(150, 23);
-            this.txtCotacaoDolar.TabIndex = 0;
-            this.txtCotacaoDolar.TextChanged += new System.EventHandler(this.txtCotacaoDolar_TextChanged);
             // 
             // txtValorEmDolar
             // 
@@ -50,10 +40,11 @@
             this.txtValorEmDolar.Size = new System.Drawing.Size(155, 23);
             this.txtValorEmDolar.TabIndex = 1;
             this.txtValorEmDolar.TextChanged += new System.EventHandler(this.txtValorEmDolar_TextChanged);
+            this.txtValorEmDolar.Leave += new System.EventHandler(this.txtValorEmDolar_Leave);
             // 
             // btnConverterEmReal
             // 
-            this.btnConverterEmReal.Location = new System.Drawing.Point(33, 121);
+            this.btnConverterEmReal.Location = new System.Drawing.Point(48, 106);
             this.btnConverterEmReal.Name = "btnConverterEmReal";
             this.btnConverterEmReal.Size = new System.Drawing.Size(123, 23);
             this.btnConverterEmReal.TabIndex = 2;
@@ -65,10 +56,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(286, 171);
+            this.ClientSize = new System.Drawing.Size(232, 171);
             this.Controls.Add(this.btnConverterEmReal);
             this.Controls.Add(this.txtValorEmDolar);
-            this.Controls.Add(this.txtCotacaoDolar);
             this.Name = "frmConversorMoeda";
             this.Text = "frmConversorMoeda";
             this.ResumeLayout(false);
@@ -77,8 +67,6 @@
         }
 
         #endregion
-
-        private TextBox txtCotacaoDolar;
         private TextBox txtValorEmDolar;
         private Button btnConverterEmReal;
     }
--- client/AplicacaoPoo.Estrutural.Windows/frmConversorMoeda.cs
+++ client/AplicacaoPoo.Estrutural.Windows/frmConversorMoeda.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AplicacaoPoo.Dominio.services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,68 +13,43 @@ namespace AplicacaoPoo.Estrutural.Windows
 {
     public partial class frmConversorMoeda : Form
     {
-        private bool CotacaoEhValido;
-        private bool ValorEmDolarEhValido;
         public frmConversorMoeda()
         {
             InitializeComponent();
-            HabilitarOuDesabilitarBotaoCalcularConversao();
+            btnConverterEmReal.Enabled = false;
         }
 
         private void btnConverterEmReal_Click(object sender, EventArgs e)
         {
-            var cotacaoDolar = decimal.Parse(txtCotacaoDolar.Text);
             var valorEmDolar = decimal.Parse(txtValorEmDolar.Text);
-
-            var valorConvertido = cotacaoDolar * valorEmDolar;
-
-            MessageBox.Show("Valor convertido em real é: " + valorConvertido,
-                "Resultado da conversão de Dolar para Real");
+            
+            var moeda = new ConverterMoedaService();
+            var resultado = moeda.ConverterDolarEmReal(valorEmDolar);
+            
+            //string interpolation
+            MessageBox.Show($"Valor convertido é: {resultado} reais");
         }
 
-        private void txtCotacaoDolar_TextChanged(object sender, EventArgs e)
-        {
-            try
-            {
-                var resultado = decimal.Parse(txtCotacaoDolar.Text);
-                CotacaoEhValido = true;
-                HabilitarOuDesabilitarBotaoCalcularConversao();
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("A cotação do dalor é um valor decimal");
-                txtCotacaoDolar.Focus();
-                CotacaoEhValido = false;
-                HabilitarOuDesabilitarBotaoCalcularConversao();
-            }
-        }
 
         private void txtValorEmDolar_TextChanged(object sender, EventArgs e)
         {
             try
             {
+                if (txtValorEmDolar.Text == "")
+                {
+                    btnConverterEmReal.Enabled = false
+                        ;
+                    return;
+                };
+
                 var resultado = decimal.Parse(txtValorEmDolar.Text);
-                ValorEmDolarEhValido = true;
-                HabilitarOuDesabilitarBotaoCalcularConversao();
+                btnConverterEmReal.Enabled = true;
             }
             catch (Exception)
             {
                 MessageBox.Show("A cotação do dalor é um valor decimal");
                 txtValorEmDolar.Focus();
-                ValorEmDolarEhValido = false;
-                HabilitarOuDesabilitarBotaoCalcularConversao();
-            }
-        }
-
-        private void HabilitarOuDesabilitarBotaoCalcularConversao()
-        {
-            if (ValorEmDolarEhValido && CotacaoEhValido)
-            {
-                btnConverterEmReal.Enabled = true;
-            }
-            else
-            {
-                btnConverterEmReal.Enabled=false;
+                btnConverterEmReal.Enabled = false;
             }
         }
     }
